271d13652516937ba08beed4a5eb1e47a0ead190
Andres Pinto <143480783+apsantiso@users.noreply.github.com>
2026-07-09 16:16:09 +0200

    chore: changed message

--- src/sub-account/views/SubAccountBucketsPage.tsx
+++ src/sub-account/views/SubAccountBucketsPage.tsx
@@ -419,7 +419,9 @@ const CreateBucketModal = ({
     const regionClient = createRegionS3Client(selectedRegion.endpoint, selectedRegion.slug, credentials);
     try {
       if (await s3Service.bucketExists(regionClient, bucketName)) {
-        notificationsService.error({ text: 'A bucket with this name already exists' });
+        notificationsService.error({
+          text: 'The requested bucket name is not available. The bucket namespace is shared by all users of the system. Please select a different name and try again.',
+        });
         return;
       }
       await s3Service.createBucket(regionClient, bucketName, selectedRegion.slug, objectLockEnabled);
