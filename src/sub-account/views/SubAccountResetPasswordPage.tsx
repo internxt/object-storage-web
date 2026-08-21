cebc048dec1c3335050ab9556105e66ff1c37a5e
Victor Fernandez <victorfernandezrobles.dev@gmail.com>
2026-08-21 17:45:55 +0200

    feat(auth): apply sub-account branding to the password recovery pages

--- src/sub-account/views/SubAccountResetPasswordPage.tsx
+++ src/sub-account/views/SubAccountResetPasswordPage.tsx
@@ -1,14 +1,21 @@
 import { ResetPasswordView } from '../../components/auth/ResetPasswordView';
 import { subAccountAuthService } from '../services/sub-account-auth.service';
 import { subAccountConsoleBranding } from '../console-branding';
+import { AuthPageSkeleton } from '../../components/auth/AuthPageSkeleton';
+import { useSubAccountBranding } from '../context/SubAccountBrandingContext/useSubAccountBranding';
 
 export const SubAccountResetPasswordPage = () => {
+  const { branding, isLoading, styles } = useSubAccountBranding();
+
+  if (isLoading) return <AuthPageSkeleton inputRows={2} />;
+
   return (
     <ResetPasswordView
       {...subAccountConsoleBranding}
       loginPath='/subaccount/login'
       forgotPasswordPath='/subaccount/forgot-password'
       resetPassword={subAccountAuthService.resetPassword}
+      branding={{ logoUrl: branding.logoUrl, styles: branding.primaryColor ? styles : undefined }}
     />
   );
 };
